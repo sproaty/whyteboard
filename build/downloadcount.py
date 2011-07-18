ce6ff63a74352d861d7454173d6d5439d0e92d6e
Steven Sproat <sproaty@gmail.com>
2011-07-18 21:30:50 +0100

    small refactor.

--- build/downloadcount.py
+++ build/downloadcount.py
@@ -18,11 +18,16 @@ results = []
 class DownloadCount(object):
     def __init__(self, website, count):
         self.website = website
+        if isinstance(count, str):
+            count = count.replace(",", "")
         self.count = int(count)
         print " {0:<25} {1:5,}".format(website, self.count)
 
+def add_download(name, count):
+    global results
+    results.append(DownloadCount(name, count))
 
-def get_soup(site):
+def get_soup(site, cssSelector=None):
     try:
         f = urllib.urlopen(site)
     except IOError:
@@ -31,19 +36,22 @@ def get_soup(site):
 
     html = f.read()
     f.close()
-    return BeautifulSoup(html)
+    soup = BeautifulSoup(html)
+    if not cssSelector:
+        return soup
+    return select(soup, cssSelector)
 
 
-#----------------------------------------------------------------------
+#---------------------------------------------------------------
 
 
 soup = get_soup("http://code.google.com/p/whyteboard/downloads/list?can=1&q=&colspec=Filename+Summary+Uploaded+Size+DownloadCount")
 
 val = 0
 for i, td in enumerate(soup.findAll("td", {"class": "vt col_4"})):
-    val += int(td.findNext('a').renderContents().strip())
+    val += int(td.findNext('a').renderContents().strip().replace(",", ""))
 
-results.append(DownloadCount('Google Code', val))
+add_download('Google Code', val)
 
 
 #----------------------------------------------------------------------
@@ -57,174 +65,161 @@ for i, td in enumerate(soup.findAll("td", {"style": "border: none; text-align: c
     launchpad += x
     val += x
 
-results.append(DownloadCount('Launchpad', launchpad))
+add_download('Launchpad', launchpad)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://sourceforge.net/project/stats/detail.php?group_id=259193&ugn=whyteboard&type=prdownload&mode=alltime")
+soup = get_soup("http://sourceforge.net/project/stats/detail.php?group_id=259193&ugn=whyteboard&type=prdownload&mode=alltime",
+                'div#doc4 div table td')
 
-table_contents = select(soup, 'div#doc4 div table td')
+sourceforge = soup[len(soup) - 2].renderContents().strip()
 
-sourceforge = table_contents[len(table_contents) - 2].renderContents().strip().replace(",", "")
-
-results.append(DownloadCount('SourceForge', sourceforge))
-
-
-#----------------------------------------------------------------------
-
-
-soup = get_soup("http://linux.softpedia.com/get/Multimedia/Graphics/Whyteboard-45071.shtml")
-
-x = select(soup, 'table#dhead td table.margin_left25px td strong')
-
-softpedia_linux = x[1].renderContents().replace(",", "")
-
-results.append(DownloadCount('Softpedia Linux', softpedia_linux))
+add_download('SourceForge', sourceforge)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.softpedia.com/get/Multimedia/Graphic/Graphic-Editors/Whyteboard.shtml")
+soup = get_soup("http://linux.softpedia.com/get/Multimedia/Graphics/Whyteboard-45071.shtml",
+                'table#dhead td table.margin_left25px td strong')
 
-x = select(soup, 'table#dhead td table.margin_left25px td strong')
+softpedia_linux = soup[1].renderContents()
 
-softpedia_windows = x[1].renderContents().replace(",", "")
-
-results.append(DownloadCount('Softpedia Windows', softpedia_windows))
-
-
-#----------------------------------------------------------------------
-
-
-soup = get_soup("http://mac.softpedia.com/get/Graphics/Whyteboard.shtml")
-
-x = select(soup, 'table#dhead td table.margin_left25px td strong')
-
-softpedia_mac = x[1].renderContents().replace(",", "")
-
-results.append(DownloadCount('Softpedia Mac', softpedia_mac))
+add_download('Softpedia Linux', softpedia_linux)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.softpedia.com/get/PORTABLE-SOFTWARE/Multimedia/Graphics/Portable-Whyteboard.shtml")
+soup = get_soup("http://www.softpedia.com/get/Multimedia/Graphic/Graphic-Editors/Whyteboard.shtml",
+                'table#dhead td table.margin_left25px td strong')
 
-x = select(soup, 'table#dhead td table.margin_left25px td strong')
+softpedia_windows = soup[1].renderContents()
 
-softpedia_portable = x[1].renderContents().replace(",", "")
-
-results.append(DownloadCount('Softpedia Portable', softpedia_portable))
+add_download('Softpedia Windows', softpedia_windows)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.computerbild.de/download/Whyteboard-6107832.html")
+soup = get_soup("http://mac.softpedia.com/get/Graphics/Whyteboard.shtml",
+                'table#dhead td table.margin_left25px td strong')
 
-x = select(soup, 'div.contentPage div.user div.content div.clearfix table tbody tr td')
+softpedia_mac = soup[1].renderContents()
 
-m = re.search('(\d{3})', x[6].renderContents().strip())
+add_download('Softpedia Mac', softpedia_mac)
+
+
+#----------------------------------------------------------------------
+
+
+soup = get_soup("http://www.softpedia.com/get/PORTABLE-SOFTWARE/Multimedia/Graphics/Portable-Whyteboard.shtml",
+                'table#dhead td table.margin_left25px td strong')
+
+softpedia_portable = soup[1].renderContents()
+
+add_download('Softpedia Portable', softpedia_portable)
+
+
+#----------------------------------------------------------------------
+
+
+soup = get_soup("http://www.computerbild.de/download/Whyteboard-6107832.html",
+                'div.contentPage div.user div.content div.clearfix table tbody tr td')
+
+m = re.search('(\d{3})', soup[6].renderContents().strip())
 
 computerbild = m.group(0)
 
 
-results.append(DownloadCount('computerbild.de', computerbild))
+add_download('computerbild.de', computerbild)
 
 
 #----------------------------------------------------------------------
 
 
 
-soup = get_soup("http://www.icewalkers.com/download/Whyteboard/3749/adl/")
+soup = get_soup("http://www.icewalkers.com/download/Whyteboard/3749/adl/",
+                'table.corptable2 td.corptd tr td div')
 
-x = select(soup, 'table.corptable2 td.corptd tr td div')
-
-m = re.search('(\d{3})', x[1].renderContents().strip())
+m = re.search('(\d{3})', soup[1].renderContents().strip())
 icewalkers = m.group(0)
 
 
-results.append(DownloadCount('Icewalkers', icewalkers))
+add_download('Icewalkers', icewalkers)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.windows7download.com/win7-portable-whyteboard/snyghjgi.html")
+soup = get_soup("http://www.windows7download.com/win7-portable-whyteboard/snyghjgi.html",
+                'div.box_white_summary_body td')
 
-x = select(soup, 'div.box_white_summary_body td')
-
-m = re.search('(\d{2})', x[4].renderContents().strip())
+m = re.search('(\d{2})', soup[4].renderContents().strip())
 windows7downloads = m.group(0)
 
-results.append(DownloadCount('windows7downloads.com', windows7downloads))
+add_download('windows7downloads.com', windows7downloads)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.downloadplex.com/tags/whyteboard/Page-1-0-0-0-0.html")
+soup = get_soup("http://www.downloadplex.com/tags/whyteboard/Page-1-0-0-0-0.html",
+                'div#ja-content div.ja-innerpad table#dl-tbl-list tbody tr.ylw td.count div.resultCount strong')
 
-x = select(soup, 'div#ja-content div.ja-innerpad table#dl-tbl-list tbody tr.ylw td.count div.resultCount strong')
+downloadplex = soup[0].renderContents()
 
-downloadplex = x[0].renderContents()
-
-results.append(DownloadCount('downloadplex', downloadplex))
+add_download('downloadplex', downloadplex)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.chip.de/downloads/Whyteboard-fuer-Linux_48294121.html")
+soup = get_soup("http://www.chip.de/downloads/Whyteboard-fuer-Linux_48294121.html",
+                'div.dl-faktbox div.dl-faktbox-row p.col2')
 
-x = select(soup, 'div.dl-faktbox div.dl-faktbox-row p.col2')
-
-m = re.search('(\d{3})', x[2].renderContents().strip())
+m = re.search('(\d{3})', soup[2].renderContents().strip())
 chip = m.group(0)
 
-results.append(DownloadCount('chip.de Linux', chip))
+add_download('chip.de Linux', chip)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.chip.de/downloads/Whyteboard_48251314.html")
+soup = get_soup("http://www.chip.de/downloads/Whyteboard_48251314.html",
+                'div.dl-faktbox div.dl-faktbox-row p.col2')
 
-x = select(soup, 'div.dl-faktbox div.dl-faktbox-row p.col2')
-
-m = re.search('(\d{4})', x[2].renderContents().strip().replace(".", ""))
+m = re.search('(\d{4})', soup[2].renderContents().strip().replace(".", ""))
 chip = m.group(0)
 
-results.append(DownloadCount('chip.de Windows', chip))
+add_download('chip.de Windows', chip)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://www.brothersoft.com/whyteboard-295735.html")
+soup = get_soup("http://www.brothersoft.com/whyteboard-295735.html",
+                'div.sever_m div.Sever1')
 
-x = select(soup, 'div.sever_m div.Sever1')
 
-
-m = re.search('(\d{2})', x[1].renderContents().strip())
+m = re.search('(\d{2})', soup[1].renderContents().strip())
 brothersoft = m.group(0)
 
-results.append(DownloadCount('Brothersoft', brothersoft))
+add_download('Brothersoft', brothersoft)
 
 
 #----------------------------------------------------------------------
 
 
-soup = get_soup("http://download.html.it/software/vedi/11171/whyteboard/")
+soup = get_soup("http://download.html.it/software/vedi/11171/whyteboard/",
+                'table.software-info tbody tr td')
 
-x = select(soup, 'table.software-info tbody tr td')
+italian = soup[2].renderContents()
 
-italian = x[2].renderContents()
-
-results.append(DownloadCount('download.html.it', italian))
+add_download('download.html.it', italian)
 
 
 #----------------------------------------------------------------------
@@ -236,3 +231,5 @@ for download in results:
 
 print '%25s ------' % " "
 print '{0:<25} {1:,}'.format(" Total", total)
+
+raw_input() # so window doesn't close when run from file explorer
